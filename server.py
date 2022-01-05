0ec051115886947307db3d69ebee038914e33177
murugeshkt <97181206+murugeshkt@users.noreply.github.com>
2022-01-05 22:13:56 +0530

    Update server.py

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ from aiohttp import web
 
 async def handle(request):      
     name = request.match_info.get('name', "World!")
+    print("Testing with github actions")
     text = "Hello, " + name
     print('received request, replying with "{}".'.format(text))
     return web.Response(text=text)
